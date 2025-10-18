624e3c44ae3ef71dc83abe9fe8164d67a0c28a10
Vanshika Agarwal <vanshikaagarwal025@gmail.com>
2025-10-18 12:50:31 +0530

    Render

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,27 +1,33 @@
 import express from "express";
 import passport from "passport";
+
 const router = express.Router();
 
+// Google login route
 router.get("/google", passport.authenticate("google", { 
   scope: ["profile", "email"],
   prompt: "select_account"
 }));
 
-
+// Google callback route
 router.get(
   "/google/callback",
   passport.authenticate("google", {
-    successRedirect: "https://quicknotes-3.onrender.com/",
-    failureRedirect: "https://quicknotes-3.onrender.com/login"
-  })
+    failureRedirect: "https://quicknotes-3.onrender.com/login",
+  }),
+  (req, res) => {
+    res.redirect("https://quicknotes-3.onrender.com/notes");
+  }
 );
 
+// Logout
 router.get("/logout", (req, res) => {
   req.logout(() => {
     res.redirect("https://quicknotes-3.onrender.com/login");
   });
 });
 
+// Check user session
 router.get("/user", (req, res) => {
   res.send(req.user || null);
 });
